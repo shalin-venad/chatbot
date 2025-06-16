4bc21930af1f4e4f05761f1c0604b13f95989bed
Shalin Varghese <shalinvenadgit@gmail.com>
2025-06-16 13:55:58 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from langchain_ollama import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+template = """
+You are a helpful AI assistant.
+
+{context}
+User: {question}
+AI:"""
+
+model = OllamaLLM(model="llama3.2:1b")
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model
+
+def convo():
+    context = ""
+    print("Welcome to AI Chatbot , type bye to exit")
+    while True:
+        user = input("User: ")
+        if user.lower() == "bye":
+            break
+        result = chain.invoke({"context":context,"question":user})
+        print("Bot: ",result)
+        context += f"\nUser: {user}\nAI: {result}"
+        
+if __name__ == "__main__":
+    convo()
